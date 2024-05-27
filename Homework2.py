3a78a2b1d773f0eca6944fd6314a9bccf41bfed6
Relissa <troshina0120@gmail.com>
2024-05-27 20:46:23 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+homework_completed = 12
+time = 1.5
+course = 'Python'
+time_for_one = time/homework_completed
+print('Курс: ' + course + ', всего задач: ' + str(homework_completed) + ', затрачено часов: ' + str(time) + ', среднее время выполнения ' + str(time_for_one) + ' часа.')
